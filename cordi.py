61c0af30a723d44d0684d55b1ba02c3544cd82b6
leninchen <leninchen@tencent.com>
2021-11-23 09:18:13 +0800

    cordi

--- cordi.py
+++ cordi.py
@@ -1,6 +1,12 @@
+import sys
 import pyautogui as pg
-import  time
+import time
+
 while True:
     x,y=pg.position()
     time.sleep(50 / 1000)
-    print("x: {0}, y: {1}".format(x,y),end="\r")
+    CURSOR_UP_ONE = '\x1b[1A'
+    ERASE_LINE = '\x1b[2K'
+    sys.stdout.write(CURSOR_UP_ONE)
+    sys.stdout.write(ERASE_LINE)
+    print("x: {0}, y: {1}".format(x,y))
