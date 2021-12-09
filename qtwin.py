97b7e0aaa535ff98bdc0553c0ceb2e61e6e4d57a
leninchen <leninchen@tencent.com>
2021-12-09 15:37:48 +0800

    win

--- qtwin.py
+++ qtwin.py
@@ -48,38 +48,58 @@ class Form(QDialog):
         self.pulling=False
         self.resize(800,800)
         # Create widgets
-        self.edit = QLineEdit("Write my name here")
-        self.button = QPushButton("Show Greetings")
+        self.edit = QLineEdit()
+        self.button = QPushButton("PULL FOR LUCK!")
         self.label= [QLabel(figure[sun]),QLabel(figure[sun]),QLabel(figure[sun])]
         self.ele=[sun,sun,sun]
+        self.winmsg=QLabel("Hello!")
+
         # Create layout and add widgets
         layout = QVBoxLayout()
         hl=QHBoxLayout()
         for l in self.label:
             hl.addWidget(l)
         layout.addLayout(hl)
-        layout.addWidget(self.edit)
+
+        hl2=QHBoxLayout()
+        hl2.addWidget(QLabel("$"))
+        hl2.addWidget(self.edit)
+
+        layout.addLayout(hl2)
+        layout.addWidget(self.winmsg)
         layout.addWidget(self.button)
+
         # Set dialog layout
         self.setLayout(layout)
-        # Add button signal to greetings slot
-        self.button.clicked.connect(self.greetings)
-        self.refresh_period=50
 
-    # Greets the user
-    def greetings(self):
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.pulled)
+        self.edit.textChanged.connect(self.textChanged)
+
+        self.refresh_period=50
+        self.setMoney(1000)
+
+
+    def setMsg(self,msg):
+        self.winmsg.setText(msg)
+
+    def setMoney(self,n):
+        self.money=n
+        self.edit.setText(str(n))
+
+    def textChanged(self):
+        self.money=int(self.edit.text())
+
+    def pulled(self):
         if not self.pulling:
             self.pulling=True
             threading.Thread(target = self.pulled_func,args=(random.sample(range(20,50),3),)).start()
 
     def pulled_func(self,scl):
-        #global money,win_text
-        # if money<=0:
-        #     win_text="you are broke!"
-        #     return
-        win_text=" "
-        # money-=10
-        #scl=random.sample(range(20,50),3)
+        if self.money<=0:
+            self.setMsg("you are broke!")
+            return
+        self.setMoney(self.money-10)
         for i,sc in enumerate(scl):
             bi=fig_list.index(self.ele[i])
             for j in range(sc):
@@ -88,8 +108,8 @@ class Form(QDialog):
                 self.label[i].setText(figure[self.ele[i]])
                 time.sleep(self.refresh_period/1000)
         if self.ele[0]==self.ele[1] and self.ele[1]==self.ele[2]:
-            win_text="you win!"
-            # money+=1000
+            self.setMsg("you win!")
+            self.setMoney(self.money+1000)
         self.pulling=False
 
 if __name__ == '__main__':
